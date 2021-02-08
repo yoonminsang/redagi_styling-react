1890498b16fea01e1e60da4cde8a777870d5e7a3
yoonminsang <apdlyooapdl@naver.com>
2021-02-08 23:46:45 +0900

    ex

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-import SassComponent from "./SassComponent";
+// import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
+// import StyledComponent from "./StyledComponent";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        {/* <StyledComponent /> */}
+        {/* <SassComponent /> */}
+        <CSSModule />
       </div>
     );
   }
